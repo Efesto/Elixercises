98b412206a2778db4aba9d1d61ab5796562d4a22
Marco Polita <marco.polita@solarisbank.de>
2020-02-17 18:14:38 +0100

    WIP on assertions framework

--- metaprogramming/assertions.exs
+++ metaprogramming/assertions.exs
@@ -1,15 +1,21 @@
 defmodule Assertions do
   def assert(:==, lhs, rhs) do
     case lhs == rhs do
-      true -> IO.write(".")
-      false -> IO.puts("ERROR: #{lhs} is not equal to #{rhs}")
+      true ->
+        IO.write("\e[32m.\e[0m")
+
+      false ->
+        IO.write("\e[31mf\e[0m")
     end
   end
 
   def assert(:>=, lhs, rhs) do
     case lhs >= rhs do
-      true -> IO.write(".")
-      false -> IO.puts("ERROR: #{lhs} is not bigger or equal to #{rhs}")
+      true ->
+        IO.write("\e[32m.\e[0m")
+
+      false ->
+        IO.write("\e[31mf\e[0m")
     end
   end
 
@@ -23,6 +29,8 @@ defmodule Assertions do
     quote do
       import unquote(__MODULE__)
       Module.register_attribute(__MODULE__, :tests, accumulate: true)
+      Module.register_attribute(__MODULE__, :async_tests, accumulate: true)
+
       @before_compile unquote(__MODULE__)
     end
   end
@@ -30,11 +38,22 @@ defmodule Assertions do
   defmacro __before_compile__(_env) do
     quote do
       def run do
-        IO.puts("Running the tests (#{inspect(@tests)})")
+        IO.puts("Running tests (#{Enum.count(@tests)})")
 
         Enum.each(@tests, fn {function, description} ->
-          IO.puts(description)
+          IO.puts("\e[1m|> #{description}\e[0m")
           apply(__MODULE__, function, [])
+        end)
+      end
+
+      def parallel_run do
+        IO.puts("Running tests in parallel(#{Enum.count(@async_tests)})")
+
+        Enum.each(@async_tests, fn {function, description} ->
+          Task.start(fn ->
+            IO.puts("\e[1m|> #{description}\e[0m")
+            apply(__MODULE__, function, [])
+          end)
         end)
       end
     end
@@ -45,7 +64,20 @@ defmodule Assertions do
 
     quote do
       @tests {unquote(test_func), unquote(description)}
-      def unquote(test_func)(), do: unquote(testing_block)
+
+      def unquote(test_func)() do
+        unquote(testing_block)
+        IO.puts("")
+      end
+    end
+  end
+
+  defmacro test(description, [async: true], do: testing_block) do
+    test_func = String.to_atom(description)
+
+    quote do
+      @async_tests {unquote(test_func), unquote(description)}
+      test(unquote(description), do: unquote(testing_block))
     end
   end
 end
@@ -53,13 +85,13 @@ end
 defmodule MathTests do
   use Assertions
 
-  test "all the numbers are the same" do
+  test "all the numbers are the same", async: true do
     assert 5 == 5
     assert 5 == 6
     assert 6 == 6
   end
 
-  test "all numbers are big" do
+  test "all numbers are big", async: true do
     assert 5 >= 6
     assert 6 >= 6
     assert 5 >= 3
