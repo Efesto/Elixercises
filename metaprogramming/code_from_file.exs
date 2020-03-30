3feed0ff402d186e8e7a4838dcd3d65aabd74872
Marco Polita <marco.polita@solarisbank.de>
2020-03-30 16:58:35 +0200

    removes deprecated code

--- metaprogramming/code_from_file.exs
+++ metaprogramming/code_from_file.exs
@@ -2,7 +2,7 @@ defmodule Enigma do
   @external_resource enigma_code_path = Path.join([__DIR__, "enigma.txt"])
 
   for line <- File.stream!(enigma_code_path, [], :line) do
-    [code1, code2] = line |> String.split(" ") |> Enum.map(&String.strip(&1))
+    [code1, code2] = line |> String.split(" ") |> Enum.map(&String.trim(&1))
 
     def encode(unquote(code1) <> rest), do: unquote(code2) <> encode(rest)
     def decode(unquote(code2) <> rest), do: unquote(code1) <> decode(rest)
